486329916bcdd3e659c6c1ff86baced52e34af69
SHIMA3desu <takafumi.matsushima@quick.jp>
2016-12-16 16:50:45 +0900

    'First'

---
+++ app/src/main/java/jp/techacademy/takafumi/matsushima/qa_app/Const.java
@@ -0,0 +1,13 @@
+package jp.techacademy.takafumi.matsushima.qa_app;
+
+/**
+ * Created by qcq0447 on 2016/12/16.
+ */
+
+public class Const {
+    public static final String UsersPATH = "users"; // Firebaseにユーザの表示名を保存するパス
+    public static final String ContentsPATH = "contents"; // Firebaseに質問を保存するバス
+    public static final String AnswersPATH = "answers"; // Firebaseに解答を保存するパス
+
+    public static final String NameKEY = "name"; // Preferenceに表示名を保存する時のキー
+}
