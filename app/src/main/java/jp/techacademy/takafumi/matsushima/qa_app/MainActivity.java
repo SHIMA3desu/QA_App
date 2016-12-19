f44a8ad89b43c1ee2fadcf5c7acdca6d7043046e
SHIMA3desu <takafumi.matsushima@quick.jp>
2016-12-19 18:43:23 +0900

    'First'

--- app/src/main/java/jp/techacademy/takafumi/matsushima/qa_app/MainActivity.java
+++ app/src/main/java/jp/techacademy/takafumi/matsushima/qa_app/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -42,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        Log.d("TESTJAVA","Menu_main PASS");
         return true;
     }
 
@@ -54,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Log.d("TESTJAVA","Action_Setting PASS");
             return true;
         }
 
